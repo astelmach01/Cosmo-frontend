1a2ce8ade41dd1e24fb6592cccbf4aa6699f108b
astelmach01 <astelmach20@gmail.com>
2023-08-21 23:04:52 -0700

    added post and progress bar successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import { TailSpin } from 'react-loader-spinner';
 import { inject } from '@vercel/analytics';
+
+const API_BASE_URL = 'https://cosmo-backend-api-b1c95546381e.herokuapp.com';
 
 inject();
 
 function App() {
   const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   const sendMessage = async (text) => {
     try {
-      await axios.post('/chat/request', { prompt: text });
-      console.log('Message sent:', text);
+      setMessage('');
+      setIsLoading(true);
+      const res = await axios.post(`${API_BASE_URL}/api/chat/request`, { prompt: text });
+      setResponse(res.data.response);
     } catch (error) {
-      console.error('Error sending message:', error);
+      setResponse({ error: error.toString() });
+    } finally {
+      setIsLoading(false);
     }
   };
+
   const handleKeyDown = async (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
@@ -25,12 +36,34 @@ function App() {
     }
   };
 
+  let responseContent = null;
+
+  if (isLoading) {
+    responseContent = (
+      <TailSpin
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible
+      />
+    );
+  } else if (typeof response === 'object') {
+    responseContent = <pre>{JSON.stringify(response, null, 2)}</pre>;
+  } else {
+    responseContent = <p>{response}</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Cosmo Chat</h1>
       </header>
       <main className="App-main">
+        <div className="response-box">{responseContent}</div>
         <div className="message-box">
           <textarea
             rows={4}

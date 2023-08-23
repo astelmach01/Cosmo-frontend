fb8b34707343d664b32cf31d78e19fe263fae511
astelmach01 <astelmach20@gmail.com>
2023-08-22 22:03:09 -0700

    fixed

--- src/message/MessageInput.jsx
+++ src/message/MessageInput.jsx
@@ -1,34 +1,38 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 
 function MessageInput({ sendMessage }) {
   const [message, setMessage] = useState('');
 
   const handleKeyDown = async (e) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      if (message.trim() !== '') {
-        await sendMessage(message);
-        setMessage('');
-      }
+    if (e.keyCode === 13 && message.trim() !== '') {
+      await sendMessage(message);
+      setMessage('');
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (message.trim() !== '') {
+      await sendMessage(message);
+      setMessage('');
     }
   };
 
   return (
-    <div className="message-box">
-      <textarea
-        rows={4}
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        onKeyDown={handleKeyDown}
-        placeholder="Type your message here..."
-      />
-    </div>
+      <div className="message-box">
+        <div className="message-input">
+        <textarea
+            rows={4}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Type your message here..."
+        />
+          <button onClick={handleSubmit} className="submit-button">
+            Submit
+          </button>
+        </div>
+      </div>
   );
 }
 
 export default MessageInput;
-
-MessageInput.propTypes = {
-  sendMessage: PropTypes.func.isRequired,
-};

572ab5fa1961538871617284913aa8ad1f091341
astelmach01 <astelmach20@gmail.com>
2023-08-31 23:26:53 -0700

    update and delete work

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
-import './ToDoList.css';  // Import the CSS
+import React, { useState } from 'react';
+import './ToDoList.css';
+import { updateUserTask, deleteUserTask } from './api';
 
-function ToDoList({ tasks, error }) {
+function ToDoList({ tasks, error, refreshTasks }) {
+    const [updatedTask, setUpdatedTask] = useState('');
+    const [updatedDate, setUpdatedDate] = useState('');
+
+    const handleDelete = async (taskId) => {
+        try {
+            await deleteUserTask(taskId);
+            refreshTasks();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleUpdate = async (taskId) => {
+        try {
+            const payload = {
+                task_id: taskId,
+                description: updatedTask
+            }
+            if (updatedDate) {
+                payload.date = updatedDate;
+            }
+            console.log("Sending payload", payload);
+            await updateUserTask(taskId, payload);
+            refreshTasks();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <div className="todo-list">
             <h2>Your Tasks</h2>
@@ -11,6 +41,10 @@ function ToDoList({ tasks, error }) {
                     <li key={index} className="todo-item">
                         <span className="task">{task.task}</span>
                         <span className="date">{task.date}</span>
+                        <button onClick={() => handleDelete(task.id)}>Delete</button>
+                        <input type="text" placeholder="New Task" onChange={(e) => setUpdatedTask(e.target.value)} />
+                        <input type="text" placeholder="New Date" onChange={(e) => setUpdatedDate(e.target.value)} />
+                        <button onClick={() => handleUpdate(task.id)}>Update</button>
                     </li>
                 ))}
             </ul>
--- src/api.js
+++ src/api.js
@@ -1,14 +1,11 @@
+// Existing imports
 import axios from 'axios';
+let API_BASE_URL = 'https://cosmo-backend-api-b1c95546381e.herokuapp.com';
 
 
-// let API_BASE_URL = 'http://localhost:8000';
-let API_BASE_URL = 'cosmo-backend-api-b1c95546381e.herokuapp.com';
-
-// Existing imports and code
-// Existing imports and code
 export const getUserTasks = async (userId) => {
     try {
-        const res = await axios.get(`${API_BASE_URL}/api/tasks/get_tasks/${userId}`);
+        const res = await axios.get(`${API_BASE_URL}/api/tasks/get_user_tasks/${userId}`);
         if (res.data.success) {
             return { success: true, tasks: res.data.tasks };
         } else {
@@ -21,3 +18,19 @@ export const getUserTasks = async (userId) => {
 
 
 export const sendRequest = async (text) => axios.post(`${API_BASE_URL}/api/chat/request`, { prompt: text });
+
+export const createTask = async (userId, taskData) => {
+    return axios.post(`${API_BASE_URL}/api/tasks/create_task/${userId}`, null, {
+        params: taskData
+    });
+};
+
+export const updateUserTask = async (taskId, updatedData) => {
+    return axios.put(`${API_BASE_URL}/api/tasks/update_task/${taskId}`, null, {
+        params: updatedData
+    });
+};
+
+export const deleteUserTask = async (taskId) => {
+    return axios.delete(`${API_BASE_URL}/api/tasks/delete_task/${taskId}`);
+};
